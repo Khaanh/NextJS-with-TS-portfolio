ae52f213d788af151ba9b64157a29da1cba8666f
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-03 23:22:17 +0100

    feat(contact): send email function

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,8 @@
 "use server";
-import { validateString } from "@/lib/utils";
+import React from "react";
+import { getErrorMessage, validateString } from "@/lib/utils";
 import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -20,15 +22,21 @@ export const sendEmail = async (formData: FormData) => {
 		};
 	}
 
+	new Error("message");
 	try {
 		await resend.emails.send({
 			from: "Contact Form <onboarding@resend.dev>",
 			to: "khanh13nguyen13@gmail.com",
 			subject: "Message from contact form",
 			reply_to: senderEmail as string,
-			text: message as string,
+			react: React.createElement(ContactFormEmail, {
+				message: message as string,
+				senderEmail: senderEmail as string,
+			}),
 		});
-	} catch (error) {
-		console.log(error);
+	} catch (error: unknown) {
+		return {
+			error: getErrorMessage(error),
+		};
 	}
 };
