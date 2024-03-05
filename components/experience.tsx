dce5fa5213bd5a689a0b94e3ab0fcce9535005db
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-05 21:17:25 +0100

    feat(darkmode): added button switch theme

--- components/experience.tsx
+++ components/experience.tsx
@@ -21,10 +21,10 @@ export default function Experience() {
 						<VerticalTimelineElement
 							visible={true}
 							contentStyle={{
-								background: "#f3f4f6",
+								background: theme === 'light' ? "#f3f4f6" : "rgba(255,255,255, 0.05)",
 								boxShadow: "none",
 								border: "1px solid rgba(0,0,0, 0.05)",
-								textAlign: "left",
+								textAlign: "left"
 								padding: "1.3rem 2rem",
 							}}
 							contentArrowStyle={{
