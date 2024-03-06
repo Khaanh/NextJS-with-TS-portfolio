6a5bdf1ab6537cb731ed81d297805f75a6a8c326
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-06 13:58:07 +0100

    fix(project): responsive

--- components/project.tsx
+++ components/project.tsx
@@ -32,8 +32,8 @@ export default function Project({
 			}}
 			className="group mb-3 sm:mb-8 last:mb-0"
 		>
-			<section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]hover:bg-gray-200 transition group-even:even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+			<section className="flex flex-col-reverse items-center sm:items-start bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]hover:bg-gray-200 transition group-even:even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-auto">
 					<h3 className="text-2xl font-semibold">{title}</h3>
 					<p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
 						{description}
@@ -54,7 +54,7 @@ export default function Project({
 					src={imageUrl}
 					alt="Project I worked on"
 					quality={95}
-					className="absolute top-8 -right-40 w-[28.25rem]  rounded-t-lg shadow-2xl 
+					className="w-full sm:w-[70%] sm:absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
 				transition 
 				group-hover:scale-[1.04]
 				group-hover:-translate-x-3
